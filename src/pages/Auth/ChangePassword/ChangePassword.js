4310badc1c73d44e1c6609df5ea86e2869f58ed4
tomg <tommeisterr@users.noreply.github.com>
2020-01-20 10:42:57 +0100

    changes

--- src/pages/Auth/ChangePassword/ChangePassword.js
+++ src/pages/Auth/ChangePassword/ChangePassword.js
@@ -3,6 +3,7 @@ import { Auth } from 'aws-amplify';
 import { useHistory } from 'react-router-dom';
 import FormErrors from '../../../components/Validation/FormErrors.js';
 import Validate from '../../../components/Validation/FormValidation.js';
+import './ChangePassword.scss';
 
 const ChangePassword = ()=> {
   const history = useHistory();
@@ -93,7 +94,7 @@ const ChangePassword = ()=> {
                   onChange={onInputChange}
                 />
                 <span className='icon is-small is-left'>
-                  <i className='fas fa-lock'></i>
+                  <i className='fas fa-lock icon'></i>
                 </span>
               </p>
             </div>
@@ -108,7 +109,7 @@ const ChangePassword = ()=> {
                   onChange={onInputChange}
                 />
                 <span className='icon is-small is-left'>
-                  <i className='fas fa-lock'></i>
+                  <i className='fas fa-lock icon'></i>
                 </span>
               </p>
             </div>
@@ -123,7 +124,7 @@ const ChangePassword = ()=> {
                   onChange={onInputChange}
                 />
                 <span className='icon is-small is-left'>
-                  <i className='fas fa-lock'></i>
+                  <i className='fas fa-lock icon'></i>
                 </span>
               </p>
             </div>
@@ -135,7 +136,7 @@ const ChangePassword = ()=> {
             <div className='field'>
               <p className='control'>
                 <button className='button is-success' type='submit'>
-                  Change password
+                  Submit
                 </button>
               </p>
             </div>
--- src/pages/NewProduct/NewProduct.js
+++ src/pages/NewProduct/NewProduct.js
@@ -52,130 +52,152 @@ const NewProduct = () => {
 	  return (
 		<div className="NewNote">
 		  <form onSubmit={handleSubmit}>
+			<div className="header-newp">
+				<i className="fas fa-arrow-left"></i>
+				<h4 className="p-header">New product</h4>
+				<div className='field'>
+				<p className='control'>
+					<button className='button is-success' type='submit'>
+						Save
+					</button>
+				</p>
+				</div>
+			</div>
+			
 		  	<div className='field'>
+				<h4 className="li-newp-name">Name*</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input-name'
 						type='text'
 						id='name'
 						aria-describedby='nameHelp'
-						placeholder='Name'
+						placeholder=''
 						value={state.name}
 						onChange={onInputChange}
 					/>
 				</p>
 			</div>
 			<div className='field'>
+			<h4 className="li-newp">Brand</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input-brand'
 						type='text'
 						id='company'
 						aria-describedby='companyHelp'
-						placeholder='Company'
+						placeholder=''
 						value={state.company === 0 ? '' : state.company}
 						onChange={onInputChange}
 					/>
 				</p>
 			</div>
 			<div className='field'>
+				<h4 className="li-newp">Calories*</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input1'
 						type='text'
 						id='kcal'
 						aria-describedby='kcalHelp'
-						placeholder='Kcal'
+						placeholder=''
 						value={state.kcal}
 						onChange={onInputChange}
 					/>
+					<h4 className="units">kcal</h4>
 				</p>
 			</div>
 			<div className='field'>
+				<h4 className="li-newp">Proteins*</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input1'
 						type='text'
 						id='proteins'
 						aria-describedby='proteinsHelp'
-						placeholder='Proteins'
+						placeholder=''
 						value={state.proteins}
 						onChange={onInputChange}
 					/>
+					<h4 className="units">g</h4>
 				</p>
 			</div>
 			<div className='field'>
+				<h4 className="li-newp">Fat*</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input1'
 						type='text'
 						id='fats'
 						aria-describedby='fatsHelp'
-						placeholder='Fats'
+						placeholder=''
 						value={state.fats}
 						onChange={onInputChange}
 					/>
+					<h4 className="units">g</h4>
 				</p>
 			</div>
 			<div className='field'>
+				<h4 className="li-newp-s">Saturated</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input1'
 						type='text'
 						id='saturated'
 						aria-describedby='saturatedHelp'
-						placeholder='Saturated'
+						placeholder=''
 						value={state.saturated === 0 ? '' : state.saturated}
 						onChange={onInputChange}
 					/>
+					<h4 className="units">g</h4>
 				</p>
 			</div>
 			<div className='field'>
+				<h4 className="li-newp">Carbs*</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input1'
 						type='text'
 						id='carbs'
 						aria-describedby='carbsHelp'
-						placeholder='Carbs'
+						placeholder=''
 						value={state.carbs}
 						onChange={onInputChange}
 					/>
+					<h4 className="units">g</h4>
 				</p>
 			</div>
 			<div className='field'>
+				<h4 className="li-newp-s">Sugars</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input1'
 						type='text'
 						id='sugars'
 						aria-describedby='sugarsHelp'
-						placeholder='Sugars'
+						placeholder=''
 						value={state.sugars === 0 ? '' : state.sugars}
 						onChange={onInputChange}
 					/>
+					<h4 className="units">g</h4>
 				</p>
 			</div>
 			<div className='field'>
+				<h4 className="li-newp">Salt</h4>
 				<p className='control'>
 					<input
-						className='input'
+						className='input1'
 						type='text'
 						id='salt'
 						aria-describedby='saltHelp'
-						placeholder='Salt'
+						placeholder=''
 						value={state.salt === 0 ? '' : state.salt}
 						onChange={onInputChange}
 					/>
+					<h4 className="units">g</h4>
 				</p>
 			</div>
-			 <div className='field'>
-				<p className='control'>
-					<button className='button is-success' type='submit'>
-						Create product
-					</button>
-				</p>
-			</div>
+			 
 			{isLoading === true ? (
 					<Loading />
 				) : 
