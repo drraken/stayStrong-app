1c62efedc61f78bd519455d3bc09fc633f91d588
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2020-01-25 17:33:42 +0100

    Env variable try nb 1

--- src/index.js
+++ src/index.js
@@ -2,34 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Amplify from 'aws-amplify';
-import config from './config';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 Amplify.configure({
 	Auth: {
 		mandatorySignId: true,
-		region: config.cognito.REGION,
-		userPoolId: config.cognito.USER_POOL_ID,
-		identityPoolId: config.cognito.IDENTITY_POOL_ID,
-		userPoolWebClientId: config.cognito.APP_CLIENT_ID
+		region: Amplify.REGION,
+		userPoolId: Amplify.USER_POOL_ID,
+		identityPoolId: Amplify.IDENTITY_POOL_ID,
+		userPoolWebClientId: Amplify.APP_CLIENT_ID
 	},
 	API:{
 		endpoints:[
 			{
 				name: 'products',
-				endpoint: config.apiGateway.URL,
-				region: config.apiGateway.REGION
+				endpoint: Amplify.URL,
+				region: Amplify.REGION
 			},
 			{
 				name: 'meals',
-				endpoint: config.apiGateway.URL,
-				region: config.apiGateway.REGION
+				endpoint: Amplify.URL,
+				region:Amplify.REGION
 			},
 			{
 				name: 'usersParameters',
-				endpoint: config.apiGateway.URL,
-				region: config.apiGateway.REGION
+				endpoint: Amplify.URL,
+				region: Amplify.REGION
 			}
 		]
 	}
